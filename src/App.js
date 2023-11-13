a75c9bdcd6dd657f8b118c80f4920654d56c949c
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-11-13 17:55:11 +0100

    mod

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MePage from "./pages/MePage";
+import Footer from "./Components/Me/footer";
+import Formazione from "./Components/Me/formazione";
+import Competenze from "./Components/Me/competenze";
+
 
 function App() {
    return (
       <BrowserRouter>
-         <div>Header</div>
+         <div></div>
          <Routes>
             <Route element={<MePage />} path="/" />
          </Routes>
-         <div>footer</div>
+         <div><Competenze/></div>
+         <div><Formazione/></div>
+         <div><Footer/></div>
       </BrowserRouter>
    );
 }
 
 export default App;
+
+// Your access token for Token Based Authentication is:
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZGI4NWM1NWU3ZTAwMThmODNiZWMiLCJpYXQiOjE2OTk4NjM0MjksImV4cCI6MTcwMTA3MzAyOX0.AeGut2RX1oz62ERdBCMBJM1Piacgla1-NhTGgCVSkJI
+
