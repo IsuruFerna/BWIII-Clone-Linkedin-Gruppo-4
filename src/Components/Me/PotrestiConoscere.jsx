77c8d3a2b3ef7ecdaf8bd96865dd66e02d2c9541
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-11-14 17:17:44 +0100

    committo al mePage perché ho già implemento qualcosa

--- src/Components/Me/PotrestiConoscere.jsx
+++ src/Components/Me/PotrestiConoscere.jsx
@@ -4,62 +4,62 @@ import SinglePerson from "./SinglePerson";
 import PublicProfile from "./PublicProfile";
 
 const PotrestiConoscere = () => {
-  const [persons, setPersons] = useState([]);
-  const getPersons = () => {
-    fetch(`https://striveschool-api.herokuapp.com/api/profile`, {
-      headers: {
-        Authorization:
-          "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWQ0ZmM1NWU3ZTAwMThmODNjMGEiLCJpYXQiOjE2OTk4Njc5ODMsImV4cCI6MTcwMTA3NzU4M30.VgwY5-_3HqrYK9L5mB8w_n1YsAqrCrIk6q-aQXvo0wU",
-      },
-    })
-      .then((datas) => {
-        console.log(datas);
-        setPersons(datas);
+   const [persons, setPersons] = useState([]);
+   const getPersons = () => {
+      fetch(`https://striveschool-api.herokuapp.com/api/profile`, {
+         headers: {
+            Authorization:
+               "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWQ0ZmM1NWU3ZTAwMThmODNjMGEiLCJpYXQiOjE2OTk4Njc5ODMsImV4cCI6MTcwMTA3NzU4M30.VgwY5-_3HqrYK9L5mB8w_n1YsAqrCrIk6q-aQXvo0wU",
+         },
       })
-      .catch((err) => console.log("Error: ", err));
-  };
-  useEffect(() => {
-    getPersons();
-  }, []);
-  return (
-    <div className="d-none d-lg-flex flex-column">
-      <Container fluid className="border rounded border-secondary mb-3">
-        <div>
-          <PublicProfile />
-        </div>
-      </Container>
-      <Container fluid className="border rounded border-secondary mb-3">
-        <div className="d-flex flex-column">
-          <h5 className="fw-bold smaller my-2 fs-6">
-            Altri profili consultati
-          </h5>
-        </div>
-        <div>
-          {persons
+         .then((datas) => {
+            console.log(datas);
+            setPersons(datas);
+         })
+         .catch((err) => console.log("Error: ", err));
+   };
+   useEffect(() => {
+      getPersons();
+   }, []);
+   return (
+      <div className="d-none d-lg-flex flex-column">
+         <Container fluid className="border rounded border-secondary mb-3">
+            <div>
+               <PublicProfile />
+            </div>
+         </Container>
+         <Container fluid className="border rounded border-secondary mb-3">
+            <div className="d-flex flex-column">
+               <h5 className="fw-bold smaller my-2 fs-6">
+                  Altri profili consultati
+               </h5>
+            </div>
+            <div>
+               {/* {persons
             .filter((allPersons, i) => i > 10 && i < 16)
             .map((singlePerson) => {
               return <SinglePerson person={singlePerson} />;
-            })}
-        </div>
-      </Container>
-      <Container fluid className="border rounded border-secondary">
-        <div className="d-flex flex-column my-2">
-          <h5 className="fw-bold smaller fs-6">
-            Persone che potresti conoscere
-          </h5>
-          <span className="color-secondary extrasmall">
-            Dalla tua scuola o università
-          </span>
-        </div>
-        <div>
-          {persons
-            .filter((allPersons, i) => i < 10)
-            .map((singlePerson) => {
-              return <SinglePerson person={singlePerson} />;
-            })}
-        </div>
-      </Container>
-    </div>
-  );
+            })} */}
+            </div>
+         </Container>
+         <Container fluid className="border rounded border-secondary">
+            <div className="d-flex flex-column my-2">
+               <h5 className="fw-bold smaller fs-6">
+                  Persone che potresti conoscere
+               </h5>
+               <span className="color-secondary extrasmall">
+                  Dalla tua scuola o università
+               </span>
+            </div>
+            <div>
+               {/* {persons
+                  .filter((allPersons, i) => i < 10)
+                  .map((singlePerson) => {
+                     return <SinglePerson person={singlePerson} />;
+                  })} */}
+            </div>
+         </Container>
+      </div>
+   );
 };
 export default PotrestiConoscere;
