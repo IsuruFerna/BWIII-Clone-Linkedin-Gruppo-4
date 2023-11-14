0a9833e68f484fa779362fa7df3dfbc7d18a1852
Angela Prestano <angiprestano99@gmail.com>
2023-11-14 11:27:13 +0100

    update

--- src/Components/Me/PotrestiConoscere.jsx
+++ src/Components/Me/PotrestiConoscere.jsx
@@ -12,6 +12,13 @@ const PotrestiConoscere = () => {
           "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWQ0ZmM1NWU3ZTAwMThmODNjMGEiLCJpYXQiOjE2OTk4Njc5ODMsImV4cCI6MTcwMTA3NzU4M30.VgwY5-_3HqrYK9L5mB8w_n1YsAqrCrIk6q-aQXvo0wU",
       },
     })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Error while getting the datas");
+        }
+      })
       .then((datas) => {
         console.log(datas);
         setPersons(datas);
