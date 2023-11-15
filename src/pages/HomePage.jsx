1959659f499a34cf7696acbdf3208e67bdd9afb5
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-11-15 16:28:47 +0100

    mod

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,22 @@
 import { Col, Container, Row } from "react-bootstrap";
+import SideBarLeftHome from "../Components/home/sideBarLeft";
 
 const HomePage = () => {
   return (
     <Container>
       <Row className="justify-content-center">
-        <Col className="col-2 bg-info">
-          Sezione a sinistra Lorem, ipsum dolor sit amet consectetur adipisicing
-          elit. Iste mollitia quis harum, rem id nihil amet. Labore ab
-          laboriosam optio iusto voluptatum quasi porro, blanditiis dolorem amet
-          eaque. Repellendus, quo!
+        <Col sm={12} md={2} className="col-md-3 col-12 bg-info">
+
+   <SideBarLeftHome />
+
         </Col>
-        <Col className="col-5 bg-warning">
+        <Col sm={12} md={5} className="col-5 bg-warning">
           Sezione centrale Lorem ipsum dolor sit amet, consectetur adipisicing
           elit. Aperiam explicabo saepe qui, ratione voluptas pariatur
           consequuntur necessitatibus ut consectetur in, veritatis aut nesciunt
           placeat, corrupti quos ullam cupiditate. Cum, magni?
         </Col>
-        <Col className="col-3 bg-danger">
+        <Col sm={12} md={3} className="col-3 bg-danger">
           Sezione a destra Lorem ipsum dolor sit amet consectetur, adipisicing
           elit. Accusamus autem soluta eius corrupti blanditiis facilis tempora
           facere, reiciendis quo recusandae, perferendis accusantium est
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,9 @@
 export const GET_USER = "GET_USER";
+export const GET_USER_EXPERIENCE = "GET_USER_EXPERIENCE";
+export const POST_USER_EXPERIENCE = "POST_USER_EXPERIENCE";
+
+const token =
+   "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWRmOGM1NWU3ZTAwMThmODNjMGUiLCJpYXQiOjE2OTk4NjgxNTIsImV4cCI6MTcwMTA3Nzc1Mn0.CBfPd6xhFicg1S-nnU0aS7XGgCdv1KkMH68c4w4c2OY";
 
 export const getUserAction = () => {
    return async (dispatch) => {
@@ -7,8 +12,7 @@ export const getUserAction = () => {
             `https://striveschool-api.herokuapp.com/api/profile/me`,
             {
                headers: {
-                  Authorization:
-                     "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWRmOGM1NWU3ZTAwMThmODNjMGUiLCJpYXQiOjE2OTk4NjgxNTIsImV4cCI6MTcwMTA3Nzc1Mn0.CBfPd6xhFicg1S-nnU0aS7XGgCdv1KkMH68c4w4c2OY",
+                  Authorization: token,
                },
             }
          );
@@ -18,8 +22,71 @@ export const getUserAction = () => {
                type: GET_USER,
                payload: fetchedUser,
             });
+
+            // if user and user ID is available, call getUserExperience with user._id
+            if (fetchedUser && fetchedUser._id) {
+               dispatch(getUserExperience(fetchedUser._id));
+            }
          } else {
             throw new Error("Error getting user!");
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   };
+};
+
+export const getUserExperience = (userId) => {
+   return async (dispatch) => {
+      try {
+         let response = await fetch(
+            `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`,
+            {
+               headers: {
+                  Authorization: token,
+               },
+            }
+         );
+         if (response.ok) {
+            let fetchedExperience = await response.json();
+
+            dispatch({
+               type: GET_USER_EXPERIENCE,
+               payload: fetchedExperience,
+            });
+         } else {
+            throw new Error("Error getting user experience!");
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   };
+};
+
+export const postUserExperience = (userId, formData) => {
+   return async (dispatch) => {
+      try {
+         let response = await fetch(
+            `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`,
+            {
+               method: "POST",
+               body: JSON.stringify(formData),
+               headers: {
+                  "Content-Type": "application/json",
+                  Authorization: token,
+               },
+            }
+         );
+         if (response.ok) {
+            let postedData = await response.json();
+
+            dispatch({
+               type: POST_USER_EXPERIENCE,
+               payload: postedData,
+            });
+            console.log("user experience posted!");
+         } else {
+            throw new Error("Error posting experience!");
          }
       } catch (error) {
          console.log(error);
