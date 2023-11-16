84c26a458d48131c57e0a277253179f0ca458750
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-11-16 19:20:30 +0100

    andrea merged

--- src/Components/Home/InputFieldForPost.jsx
+++ src/Components/Home/InputFieldForPost.jsx
@@ -2,65 +2,85 @@ import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const InputFieldForPost = () => {
-  const clearInput = () => {
-    setInput("");
-  };
+   const [image, setImage] = useState("");
+   const [input, setInput] = useState("");
 
-  const [input, setInput] = useState("");
-  const fetchPost = () => {
-    fetch(`https://striveschool-api.herokuapp.com/api/posts/`, {
-      headers: {
-        Authorization:
-          "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWQ0ZmM1NWU3ZTAwMThmODNjMGEiLCJpYXQiOjE2OTk4Njc5ODMsImV4cCI6MTcwMTA3NzU4M30.VgwY5-_3HqrYK9L5mB8w_n1YsAqrCrIk6q-aQXvo0wU",
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({ text: input }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          console.log(res);
-        } else {
-          throw new Error("Error while posting ");
-        }
+   const clearInput = () => {
+      setInput("");
+   };
+
+   const clearImage = () => {
+      setImage("");
+   };
+
+   const fetchPost = () => {
+      fetch(`https://striveschool-api.herokuapp.com/api/posts/`, {
+         headers: {
+            Authorization:
+               "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTUxZWQ0ZmM1NWU3ZTAwMThmODNjMGEiLCJpYXQiOjE2OTk4Njc5ODMsImV4cCI6MTcwMTA3NzU4M30.VgwY5-_3HqrYK9L5mB8w_n1YsAqrCrIk6q-aQXvo0wU",
+            Accept: "application/json",
+            "Content-Type": "application/json",
+         },
+         method: "POST",
+         body: JSON.stringify({ text: input }),
       })
-      .catch((err) => console.log("Error: ", err));
-  };
-  return (
-    <Form className="shadow pt-3">
-      <Form.Group className="mb-3 d-flex flex-column">
-        <Form.Control
-          type="text"
-          placeholder="Share what you're thinking with your friends"
-          className="rounded-0 border-0 p-3"
-          id="inputFieldInForm"
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-        />
-        <div className="d-flex justify-content-end px-2 align-items-center my-1">
-          <Button
-            id="cancelBtn"
-            className="rounded-1 py-0 px-3"
-            onClick={() => clearInput()}
-          >
-            Cancel
-          </Button>
-          <Button
-            id="addBtn"
-            className="rounded-1 py-0 px-3 ms-1 "
-            onClick={() => {
-              fetchPost();
-              clearInput();
-            }}
-          >
-            Post
-          </Button>
-        </div>
-      </Form.Group>
-    </Form>
-  );
+         .then((res) => {
+            if (res.ok) {
+               console.log(res);
+            } else {
+               throw new Error("Error while posting ");
+            }
+         })
+         .catch((err) => console.log("Error: ", err));
+   };
+   return (
+      <Form className="shadow pt-3">
+         <Form.Group className="mb-3 d-flex flex-column">
+            <Form.Control
+               type="text"
+               placeholder="Share what you're thinking with your friends"
+               className="rounded-0 border-0 p-3"
+               id="inputFieldInForm"
+               value={input}
+               onChange={(e) => {
+                  setInput(e.target.value);
+               }}
+            />
+            <Form.Control
+               type="text"
+               placeholder="Copy your Image Url here"
+               className="rounded-0 border-0 p-3"
+               id="inputFileInForm"
+               value={image}
+               onChange={(e) => {
+                  setImage(e.target.value);
+               }}
+            />
+            <div className="d-flex justify-content-end px-2 align-items-center my-1">
+               <Button
+                  id="cancelBtn"
+                  className="rounded-1 py-0 px-3"
+                  onClick={() => {
+                     clearInput();
+                     clearImage();
+                  }}
+               >
+                  Cancel
+               </Button>
+               <Button
+                  id="addBtn"
+                  className="rounded-1 py-0 px-3 ms-1 "
+                  onClick={() => {
+                     fetchPost();
+                     clearInput();
+                     clearImage();
+                  }}
+               >
+                  Post
+               </Button>
+            </div>
+         </Form.Group>
+      </Form>
+   );
 };
 export default InputFieldForPost;
--- src/Components/Home/SinglePost.jsx
+++ src/Components/Home/SinglePost.jsx
@@ -1,40 +1,45 @@
 import { Link } from "react-router-dom";
 import Comment1 from "./Comment1";
+import "./SinglePost.css";
 
 const SinglePost = ({ data }) => {
-  return (
-    <section
-      className=" my-3 border-secondary bg-white rounded shadow"
-      id="singlePost"
-    >
-      <section className=" p-1 d-flex align-items-center">
-        <div className="d-flex align-items-center">
-          <img
-            src={data.user.image}
-            alt="userimg"
-            style={{ width: "50px", height: "50px" }}
-            className="rounded-circle"
-          />
-        </div>
-        <div className="fw-bold ms-3">
-          <Link to={`/profile/${data.user._id}`} className="nav-link">
-            {data.user.name} {data.user.surname}
-            <span className="usernameInPosts extrasmall px-2">
-              {data.user.username}
-            </span>
-          </Link>
-        </div>
+   return (
+      <section
+         className=" my-3 border-secondary bg-white rounded shadow"
+         id="singlePost"
+      >
+         <section className=" p-1 d-flex align-items-center">
+            <div className="d-flex align-items-center">
+               <img
+                  src={data.user.image}
+                  alt="userimg"
+                  style={{ width: "50px", height: "50px" }}
+                  className="rounded-circle"
+               />
+            </div>
+            <div className="fw-bold ms-3">
+               <Link to={`/profile/${data.user._id}`} className="nav-link">
+                  {data.user.name} {data.user.surname}
+                  <span className="usernameInPosts extrasmall px-2">
+                     {data.user.username}
+                  </span>
+               </Link>
+            </div>
+         </section>
+         <section>
+            <div className="px-2 py-2 shadow-sm mt-2 rounded d-flex flex-column align-items-center">
+               <div className="post-photo mb-2">
+                  {data.image && (
+                     <img src={data.image} alt="immagine caricata" />
+                  )}
+               </div>
+               <div className="d-flex align-self-lg-start ">{data.text}</div>
+            </div>
+         </section>
+         <section>
+            <Comment1 />
+         </section>
       </section>
-      <section>
-        <div className="d-flex justify-content-center align-items-center">
-          {data.image ? <img src={data.image} alt="imaged" fluid /> : null}
-        </div>
-        <div className="px-2 py-1 mt-2 rounded px-2">{data.text}</div>
-      </section>
-      <section>
-        <Comment1 />
-      </section>
-    </section>
-  );
+   );
 };
 export default SinglePost;
