e0b1690f9edf13147da33a8ae68a58353518932a
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-11-17 09:22:46 +0100

    Create MessaggisticaNav.jsx

---
+++ src/Components/NavBarComponents/MessaggisticaNav.jsx
@@ -0,0 +1,11 @@
+import { ChatDotsFill } from "react-bootstrap-icons";
+
+const MessaggisticaNav = () => {
+  return (
+    <div className="text-secondary justify-content-center align-items-center d-flex flex-column pointer mx-1">
+      <ChatDotsFill className="fs-5 " />
+      <h6>Messaggistica</h6>
+    </div>
+  );
+};
+export default MessaggisticaNav;
