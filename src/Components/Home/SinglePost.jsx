6dfa2fb703149917b29081d13702cbb07bce4637
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-11-17 09:18:57 +0100

    Update SinglePost.jsx

--- src/Components/Home/SinglePost.jsx
+++ src/Components/Home/SinglePost.jsx
@@ -1,14 +1,18 @@
-import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+<<<<<<< HEAD
 import CommentTwo from "./commentTwo";
 
+=======
+import Comment1 from "./Comment1";
+>>>>>>> marco's-branch
 
 const SinglePost = ({ data }) => {
   return (
-    <Container
-      className="my-3 py-1 border-secondary bg-white rounded shadow"
+    <section
+      className=" my-3 border-secondary bg-white rounded shadow"
       id="singlePost"
     >
+<<<<<<< HEAD
       <div>
         <section className="d-flex align-items-center ">
           <div>
@@ -37,6 +41,36 @@ const SinglePost = ({ data }) => {
        
       </div>
     </Container>
+=======
+      <section className=" p-1 d-flex align-items-center">
+        <div className="d-flex align-items-center">
+          <img
+            src={data.user.image}
+            alt="userimg"
+            style={{ width: "50px", height: "50px" }}
+            className="rounded-circle"
+          />
+        </div>
+        <div className="fw-bold ms-3">
+          <Link to={`/profile/${data.user._id}`} className="nav-link">
+            {data.user.name} {data.user.surname}
+            <span className="usernameInPosts extrasmall px-2">
+              {data.user.username}
+            </span>
+          </Link>
+        </div>
+      </section>
+      <section>
+        <div className="d-flex justify-content-center align-items-center">
+          {data.image ? <img src={data.image} alt="imaged" fluid /> : null}
+        </div>
+        <div className="px-2 py-1 mt-2 rounded px-2">{data.text}</div>
+      </section>
+      <section>
+        <Comment1 />
+      </section>
+    </section>
+>>>>>>> marco's-branch
   );
 };
 export default SinglePost;
