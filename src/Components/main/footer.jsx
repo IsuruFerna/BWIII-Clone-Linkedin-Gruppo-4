3c59c862ed42be6c0640e20e695ea7250314f65c
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-11-17 09:19:01 +0100

    Update footer.jsx

--- src/Components/main/footer.jsx
+++ src/Components/main/footer.jsx
@@ -5,9 +5,8 @@ import { Nav, NavDropdown, Dropdown } from "react-bootstrap";
 const Footer = () => {
   return (
     <Container className="container-fluid">
-      <Row>
-        <Col className="col-2"></Col>
-        <Col className="col-8">
+      <Row className="justify-content-center">
+        <Col className="col-9">
           <Row>
             <Col
               sm={5}
@@ -16,7 +15,6 @@ const Footer = () => {
               className=" d-flex flex-column mt-sm-4 mt-md-0"
             >
               <span className="span">
-                {" "}
                 Informazioni Linee guida della community
               </span>
 
