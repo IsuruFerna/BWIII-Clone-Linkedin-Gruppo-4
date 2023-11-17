ddb394de757ce837bf77aef0527edc71dfc4155b
AndreaMarchica <andrea.marchica94@gmail.com>
2023-11-17 16:09:55 +0100

    other changes

--- src/Components/Home/SectionRight.jsx
+++ src/Components/Home/SectionRight.jsx
@@ -15,58 +15,58 @@ const SectionRight = () => {
       <Card className="mb-5 rounded-3 shadow">
         <Card.Title className="d-flex justify-content-between align-items-center me-3 fw-semibold fs-6 mt-2 ms-2">
           <div> LindedIn Notizie </div>
-          <InfoSquareFill className="" />
+          <InfoSquareFill className=" pointer" />
         </Card.Title>
-        <Card.Text className="mt-2 fw-semibold text-dark mb-1 smaller ms-1 ">
+        <Card.Text className="mt-2 fw-semibold text-dark mb-1 smaller ms-1  pointer">
           <RecordFill className="extrasmall text-dark mx-1" /> Assunzioni e
           nuovi premi
         </Card.Text>
         <p className="ms-4 text-secondary fw-light extrasmall">2 ore fa</p>
-        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
           <RecordFill className="extrasmall text-dark  mx-1" /> Le esperienze
           trainano il lusso
         </Card.Text>
         <p className="ms-4 extrasmall fw-light text-secondary">
           1 ora fa -415 lettori
         </p>
-        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
           <RecordFill className="extrasmall text-dark mx-1" /> L'immobiliare è
           sempre più tech
         </Card.Text>
         <p className="ms-4 extrasmall fw-light text-secondary">5 ore fa</p>
-        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
           <RecordFill className="extrasmall text-dark mx-1" /> Effetto ATP
           Finals per Torino
         </Card.Text>
         <p className="ms-4 extrasmall fw-light text-secondary ">2 ora fa</p>
-        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+        <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
           <RecordFill className="extrasmall text-dark mx-1" /> Il caso di 'C'è
           ancora domani'
         </Card.Text>
         <p className="ms-4 extrasmall fw-light text-secondary ">1 ore</p>
         {showMore === true ? (
           <>
-            <Card.Text className="mt-2 fw-semibold text-dark mb-1 smaller ms-1 ">
+            <Card.Text className="mt-2 fw-semibold text-dark mb-1 smaller ms-1 pointer ">
               <RecordFill className="extrasmall text-dark mx-1" /> Manuale di
               critica costruttiva
             </Card.Text>
             <p className="ms-4 text-secondary fw-light extrasmall">3 ore fa</p>
-            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
               <RecordFill className="extrasmall text-dark  mx-1" /> Le Telco
               peggiorano ancora
             </Card.Text>
             <p className="ms-4 extrasmall fw-light text-secondary">5 ore fa</p>
-            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
               <RecordFill className="extrasmall text-dark mx-1" /> I Top MBA
               negli USA
             </Card.Text>
             <p className="ms-4 extrasmall fw-light text-secondary">5 ore fa</p>
-            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
               <RecordFill className="extrasmall text-dark mx-1" /> Premi Nobel
               nel 2023
             </Card.Text>
             <p className="ms-4 extrasmall fw-light text-secondary ">4 ora fa</p>
-            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1">
+            <Card.Text className="fw-semibold text-dark mb-1 smaller ms-1 pointer">
               <RecordFill className="extrasmall text-dark mx-1" /> Assunzioni
               part-time nella regione Lazio
             </Card.Text>
--- src/Components/Me/Experience.jsx
+++ src/Components/Me/Experience.jsx
@@ -8,31 +8,31 @@ import SingleExperience from "./SingleExperience";
 import { Link } from "react-router-dom";
 
 const Experiences = () => {
-   return (
-      <Container fluid className="mb-3">
-         <Row>
-            <Col className="col-12">
-               <Card className="shadow">
-                  <Card.Body>
-                     <div className="d-flex justify-content-between align-items-center">
-                        <Card.Title>Esperienza</Card.Title>
-                        <div className="d-flex">
-                           <div className="btn-circle p-0 me-1">
-                              <PlusLg className="fs-4" />
-                           </div>
-                           <Link to="experience" className="btn-circle">
-                              <Pencil className="fs-3 text-secondary" />
-                           </Link>
-                        </div>
-                     </div>
+  return (
+    <Container fluid className="mb-3">
+      <Row>
+        <Col className="col-12">
+          <Card className="shadow">
+            <Card.Body>
+              <div className="d-flex justify-content-between align-items-center">
+                <Card.Title>Esperienza</Card.Title>
+                <div className="d-flex">
+                  <Link to="experience" className="btn-circle">
+                    <Pencil className="fs-3 text-secondary" />
+                  </Link>{" "}
+                  <div className="btn-circle p-0 me-1">
+                    <PlusLg className="fs-4" />
+                  </div>
+                </div>
+              </div>
 
-                     <SingleExperience />
-                  </Card.Body>
-               </Card>
-            </Col>
-         </Row>
-      </Container>
-   );
+              <SingleExperience />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Experiences;
