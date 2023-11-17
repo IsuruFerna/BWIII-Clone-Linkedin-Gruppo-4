dea3d2f5778e376128cb12e3c51045ba833549cc
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-11-17 09:23:00 +0100

    Create ReteNav.jsx

---
+++ src/Components/NavBarComponents/ReteNav.jsx
@@ -0,0 +1,10 @@
+import { PeopleFill } from "react-bootstrap-icons";
+
+const ReteNav = () => {
+  return (
+    <div className="text-secondary d-flex flex-column ms-3 pointer">
+      <PeopleFill className="fs-5 ms-1" /> <h6 className="me-4">Rete</h6>
+    </div>
+  );
+};
+export default ReteNav;
